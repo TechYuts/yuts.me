a3307d512755a2c938feadc9678162b5affcaa96
yuts <yutayandc@gmail.com>
2025-01-14 20:04:44 +0900

    add style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,31 @@ import React from "react";
 
 const IndexPage: React.FC = () => {
   return (
-    <div style={styles.container}>
-      <h1 className="font-sans" style={styles.heading}>Yuts</h1>
-      <p className="font-mono" style={styles.subheading}>Web Developer</p>
+    <div style={styles.outerContainer}>
+      <div style={styles.innerContainer}>
+        <h1 className="font-sans" style={styles.heading}>Yuta Yanagisawa</h1>
+        <p className="font-mono" style={styles.subheading}>Fullstack Web Developer</p>
+      </div>
     </div>
   );
 };
 
 const styles = {
-  container: {
+  outerContainer: {
     height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "column" as const,
     background: "linear-gradient(135deg, #121212, #282828, #383838)",
     color: "#e0e0e0",
     margin: 0,
+  },
+  innerContainer: {
+    width: "1000px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column" as const,
   },
   heading: {
     fontSize: "4rem",
