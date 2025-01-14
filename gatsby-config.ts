5133415a6b42aef5597a866f109ec49e03da191d
yuts <yutayandc@gmail.com>
2025-01-14 19:23:11 +0900

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,6 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-transformer-remark",
